132f83f71cfa6896f0faf88b0b70b84a57b6810d
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2017-09-09 17:39:10 +0530

    Create condition.c

---
+++ condition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scannf("%d",&a);
+if(a%2==0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+}
