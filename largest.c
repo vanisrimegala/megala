6333552f0ad906b722b89da01d96dc2f2a051846
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2017-09-10 14:25:27 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+int a,b,c;
+printf("the numbers are:");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+printf("%d",a is big");
+elseif(b>c&&b>a)
+printf("%d",b is big);
+else
+printf("%d",c is big);
+}
