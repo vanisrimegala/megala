5fdb7806d132a9d620aab6be5a74edc9be1dba14
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-01-11 02:29:07 -0800

    Create sumofk.c

---
+++ sumofk.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+add(int,int);
+int main()
+{
+int a1[10],a2[10];
+printf("The elements are");
+scanf("%d",&a1);
+printf("the elements are");
+scanf("%d",&a2);
+sum=add();
+}
+add(int a,int b)
+{
+int c;
+printf("the numbers are");
+scanf("%d",a2);
+c=a+b;
+printf("the sum is %d",c);
+	return 0;
+}
