0d091c91db06ca8a7d643f03564f0dbd8ab4a7d9
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-01-11 02:37:22 -0800

    Create sok.c

---
+++ sok.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+add(int,int);
+int main()
+{
+int a1[5]={1,2,3,4,5};
+int a2[3]={1,2,3};
+printf("The elements are");
+scanf("%d",&a1);
+printf("the elements are");
+scanf("%d",&a2);
+sum=add();
+}
+add(int a,int b)
+{
+int c;
+printf("the numbers are");
+scanf("%d",a2);
+c=a+b;
+printf("the sum is %d",c);
+	return 0;
+}
