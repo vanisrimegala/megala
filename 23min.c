c7db5cfb5ea43a115144dcdbba723e5a1dd9d0c7
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-01-19 15:53:38 +0530

    Create 23min.c

---
+++ 23min.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,a[5],i;
+printf("Enter number");
+scanf("%d",&n);
+printf("array elements are");
+for(i=0;i<=5;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=5;i++)
+{
+if(n>a[i])
+printf("%d is max",n);
+else
+printf("%d is min",n);
+}
+return 0;
