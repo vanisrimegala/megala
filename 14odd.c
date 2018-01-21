a2abc35a5217a31bf2b4007cf2f16399d08179c3
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-01-21 06:41:46 +0000

    Update 14odd.c

--- 14odd.c
+++ 14odd.c
@@ -2,15 +2,18 @@
 int main()
 {
 int n1,n2,i;
-printf("Enter the intervals");
+printf("Enter the intervals \n");
 scanf("%d%d",&n1,&n2);
 for(i=n1;i<=n2;i++)
 {
 if(i%2==0)
 {
 printf("%d is even",i);
+}
 else
-printf("%d is odd",i);
+{
+  printf("%d is odd",i);
+}
 }
 return 0;
 }
