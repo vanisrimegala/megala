7664b3ce18095d549262a59f52eaece5cc9260f7
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-01-21 04:41:23 +0000

    Update 16prime.c

--- 16prime.c
+++ 16prime.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 int main()
 {
-int num,num1,i;
+int num,num1,i,j;
 printf("Enter the number");
 scanf("%d%d",&num,&num1);
   for(i=num;i<=num1;i++)
   {
-for(j=2;j<=i/2;i++)
+for(j=2;j<=i/2;j++)
 {
 if(i%j==0)
 {
   break;
 }
 }
-  }
   if(j>i/2)
 printf("%d is prime",i);
 else
 printf("%d is not prime",i);
+  }
 return 0;
 }
 
