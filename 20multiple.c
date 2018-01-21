5b9a02b5b1edc027159c02a709a7f49496c5d22a
vanisrimegala <30829842+vanisrimegala@users.noreply.github.com>
2018-01-21 06:14:25 +0000

    Update 20multiple.c

--- 20multiple.c
+++ 20multiple.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int n,i;
+int n,i,mul=0;
 printf("Enter the number");
 scanf("%d",&n);
 for(i=1;i<=5;i++)
 {
-mul=n+n;
-printf("%d",mul);
-n++;
+mul=mul+n;
+printf("%d \n",mul);
 }
   return 0;
 }
